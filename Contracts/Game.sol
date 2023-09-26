49c3ffd30fd07f6d37def7f2b21b99c332274fab
0xSorcerers <121747420+0xsorcerers@users.noreply.github.com>
2023-09-26 12:57:13 +0100

    Update Game.sol

--- Contracts/Game.sol
+++ Contracts/Game.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-// @title AiGAME token for battledogs Arena
+// @title GAME token for battledogs Arena
 // https://twitter.com/0xSorcerers | https://github.com/Dark-Viper | https://t.me/Oxsorcerer | https://t.me/battousainakamoto | https://t.me/darcViper
 
 pragma solidity ^0.8.17;
